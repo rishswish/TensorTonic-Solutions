ffb6aa23af3b97a57ddf093183a60981b6a8cf8f
Rishabh Patil <89961075+rishswish@users.noreply.github.com>
2026-09-22 18:05:12 -0700

    Solve numpy-filter-extract

--- study-plans/numpy-basics/numpy-filter-extract/numpy-filter-extract.py
+++ study-plans/numpy-basics/numpy-filter-extract/numpy-filter-extract.py
@@ -6,11 +6,5 @@ def filter_and_extract(data: list, row_start: int, row_stop: int, threshold: flo
     """
     data=np.array(data,dtype=np.float64)
     f_data=data[row_start:row_stop,:]
-    rows,cols=f_data.shape
-    ans=[]
-    for i in range(rows):
-        for j in range(cols):
-            if f_data[i,j]>threshold:
-                ans.append(f_data[i,j])
 
-    return np.array(ans,dtype=np.float64)
+    return f_data[f_data>threshold]
